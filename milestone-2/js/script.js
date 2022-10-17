52180e7c4c771859f75b7d6868f222b4fd500bfe
SaverioCascelli <saverio.cascelli@gmail.com>
2022-10-17 16:04:19 +0200

    [add] function of bottom arrow

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -3,6 +3,7 @@ const imageSource = ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg"];
 const album = document.querySelector(".album");
 let imgCounter = 0;
 const topButton = document.querySelector(".arrow.top");
+const bottomButton = document.querySelector(".arrow.bottom");
 
 for(let i = 0 ; i < imageSource.length; i++ ){
     album.innerHTML += `<img class="sc-img d-none" src="../img/${imageSource[i]}" alt="">`;
@@ -18,6 +19,16 @@ topButton.addEventListener("click" , function(){
         imgCounter = imageSource.length-1;
     }else{
         imgCounter--;
+    }
+    imgHtmlArr[imgCounter].classList.remove("d-none");
+})
+
+bottomButton.addEventListener("click" , function(){
+    imgHtmlArr[imgCounter].classList.add("d-none");
+    if(imgCounter === imageSource.length-1){
+        imgCounter = 0;
+    }else{
+        imgCounter++;
     }
     imgHtmlArr[imgCounter].classList.remove("d-none");
 })
