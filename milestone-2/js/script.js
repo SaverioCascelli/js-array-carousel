5d6db7407abc4bcfb911d4764e2ce8e168c100f4
SaverioCascelli <saverio.cascelli@gmail.com>
2022-10-17 15:53:09 +0200

    [add] show first element of album in html

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -1,9 +1,15 @@
 const imageSource = ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg"];
 
 const album = document.querySelector(".album");
+let imgCounter = 0;
 
-
-for(let i = 0 ; i < imageSource.length-1; i++ ){
-    album.innerHTML = `<img src="../img/${imageSource[i]}" alt="">`;
+for(let i = 0 ; i < imageSource.length; i++ ){
+    album.innerHTML += `<img class="sc-img d-none" src="../img/${imageSource[i]}" alt="">`;
 }
+
+const imgHtmlArr = document.getElementsByClassName("sc-img");
+
+imgHtmlArr[imgCounter].classList.remove("d-none");
+
+
 
