67417831e831fac615bd081cd4eea8458e23b26b
SaverioCascelli <saverio.cascelli@gmail.com>
2022-10-17 16:02:48 +0200

    [add] top arrow key function

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -2,6 +2,7 @@ const imageSource = ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg"];
 
 const album = document.querySelector(".album");
 let imgCounter = 0;
+const topButton = document.querySelector(".arrow.top");
 
 for(let i = 0 ; i < imageSource.length; i++ ){
     album.innerHTML += `<img class="sc-img d-none" src="../img/${imageSource[i]}" alt="">`;
@@ -11,5 +12,13 @@ const imgHtmlArr = document.getElementsByClassName("sc-img");
 
 imgHtmlArr[imgCounter].classList.remove("d-none");
 
-
+topButton.addEventListener("click" , function(){
+    imgHtmlArr[imgCounter].classList.add("d-none");
+    if(imgCounter === 0){
+        imgCounter = imageSource.length-1;
+    }else{
+        imgCounter--;
+    }
+    imgHtmlArr[imgCounter].classList.remove("d-none");
+})
 
